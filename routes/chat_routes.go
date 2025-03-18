d499ee023ccf5925b8754745a46fc657ebe62f3e
thoulee <thou_lee@outlook.com>
2025-03-18 21:41:49 +0800

    refactor: 移除不再使用的JWT认证中间件，简化聊天路由设置

--- routes/chat_routes.go
+++ routes/chat_routes.go
@@ -3,12 +3,10 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/thoulee21/go-learn/controllers"
-	"github.com/thoulee21/go-learn/middlewares"
 )
 
 func SetupChatRoutes(r *gin.Engine, cc *controllers.ChatController) {
 	chatGroup := r.Group("/chat")
-	chatGroup.Use(middlewares.AuthJWTMiddleware())
 	{
 		chatGroup.POST("", cc.Chat)
 		chatGroup.POST("/stream", cc.StreamChat)
